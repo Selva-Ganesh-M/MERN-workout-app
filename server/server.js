9711fb49ea003e8f66a18342a21cf6a6196b1135
Selva Ganesh M <selvadev2k@gmail.com>
2022-12-02 11:38:02 +0530

    establish connection with the mongodb

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const { application } = require("express");
+const mongoose = require("mongoose");
 const express = require("express");
 const server = express();
 const workoutsRouter = require("./routes/workouts");
@@ -14,6 +14,11 @@ server.use((req, res, next) => {
 
 server.use("/api/workouts", workoutsRouter);
 
-server.listen(process.env.PORT, () => {
-  console.log("server is started at " + process.env.PORT);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() =>
+    server.listen(process.env.PORT, () => {
+      console.log("db connected & server is started at " + process.env.PORT);
+    })
+  )
+  .catch((err) => console.log(err.message));
